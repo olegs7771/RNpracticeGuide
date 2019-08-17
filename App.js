749fd3495d85d2304a741c4af72b3662fe4b7d08
Oleg Smushkevich <olegs777@bezeqint.net>
2019-08-17 15:09:49 +0300

    ok

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class App extends Component {
+  state = {
+    placeName: ""
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.textInput}
+          value={this.state.placeName.name}
+          onChangeText={text => this.setState({ placeName: text })}
+          placeholder="fill all fields"
+        />
+      </View>
+    );
+  }
 }
-
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 45,
+    justifyContent: "flex-start",
+
+    alignItems: "center",
+    backgroundColor: "#dae8e8"
   },
+  textInput: {
+    borderColor: "black",
+    borderWidth: 1,
+    width: "60%",
+
+    paddingLeft: 60
+  }
 });
