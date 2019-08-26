26e238d0a404f8030c6031538b0ed46df89d91df
Oleg Smushkevich <olegs777@bezeqint.net>
2019-08-26 20:56:50 +0300

    ok

--- App.js
+++ App.js
@@ -2,34 +2,33 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import Home from "./app/components/Home/Home";
-import Login from './app/Auth/Login';
-import Signup from './app/Auth/Signup';
-
+import Login from "./app/Auth/Login";
+import Signup from "./app/Auth/Signup";
 
 import { Provider } from "react-redux";
 import configureStore from "./store";
 const store = configureStore();
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
-const AppNavigator=createStackNavigator({
-  HomeRT:{
-screen:Home
+const AppNavigator = createStackNavigator(
+  {
+    HomeRT: {
+      screen: Home
+    },
+    LoginRT: {
+      screen: Login
+    },
+    SignupRT: {
+      screen: Signup
+    }
   },
-  LoginRT:{
-    screen:Login
-  },
-  SignupRT:{
-    screen:Signup
-  },
-  
-  
-},
-{
-  initialRouteName:'HomeRT'
-})
-let Navigation=createAppContainer(AppNavigator)
+  {
+    initialRouteName: "HomeRT"
+  }
+);
+let Navigation = createAppContainer(AppNavigator);
 
- export default  class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
@@ -45,5 +44,3 @@ const styles = StyleSheet.create({
     flex: 1
   }
 });
-
-
